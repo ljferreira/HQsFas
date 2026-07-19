973c1885d82ea80dde7708a15012bcdd735c6710
Luciano J. Ferreira <ljferreira512@gmail.com>
2026-07-19 14:54:40 -0300

    recurso do botão de menu na página home

--- site-HQsFas/js/services/viacep.js
+++ site-HQsFas/js/services/viacep.js
@@ -1,38 +1,27 @@
 export async function buscarCep(cep){
-    /*let cepLimpo = cep.replace(/\D/g, '')
+    
+    let resposta = null;
+    let dados = null;
 
-    if(cepLimpo.length !== 8) {
-        throw new Error('O CEP deve ter 8 dígitos.')
+    try{
+
+        resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        
+        if ( !resposta.ok ) {
+
+            console.log( `Http erro: ${resposta.status}` );
+            return;
+
+        }
+
+        dados = await resposta.json()
+
+    } catch( erro  ) {
+
+        throw new Error( "Houve um provável erro de rede!" );
+
     }
 
-    let resposta = await fetch(`https://viacep.com.br/ws/${cepLimpo}/json/`)*/
-    let resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    let dados = await resposta.json()
-
-    /*if(dados.erro) {
-        throw new Error ('CEP não encontrado.')
-    }*/
-
     return dados
+    
 }
-
-//import { listaPersonagem } from "./superhero.js";
-
-//let dadosIdNomePersonagens = [];
-
-// async function exibirPersonagens() {
-
-//     let personagens = await listaPersonagem();
-//     console.table( personagens );
-//     console.log("TESTANDO");
-
-// }
-
-/*window.exibirPersonagens = async function () {
-    const personagens = await listaPersonagem();
-    console.table(personagens);
-    console.log("TESTANDO");
-};
-
-function testeCons() { console.log("teste")}*/
-
